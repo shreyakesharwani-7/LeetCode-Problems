099639dafc3643a4d079090c8736a5dd0787f4c4
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2025-12-04 12:39:50 +0530

    Time: 0 ms (100%), Space: 42.7 MB (27.01%) - LeetHub

---
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -0,0 +1,33 @@
+class MyQueue {
+    Stack<Integer> s1;
+    Stack<Integer> s2;
+
+    public MyQueue() {
+        s1 = new Stack<>();
+        s2 = new Stack<>();
+    }
+
+    public void push(int x) {
+        while(!s1.isEmpty()) {
+            s2.push(s1.peek());
+            s1.pop();
+        }
+        s1.push(x);
+        while(!s2.isEmpty()) {
+            s1.push(s2.peek());
+            s2.pop();
+        }
+    }
+
+    public int pop() {
+        return s1.pop();
+    }
+
+    public int peek() {
+        return s1.peek();
+    }
+
+    public boolean empty() {
+        return s1.isEmpty();
+    }
+}
