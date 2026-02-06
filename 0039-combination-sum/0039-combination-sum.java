011d491a4ce36f8ff2e39dd09eecfd5638e889df
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2026-02-06 20:56:55 +0530

    Time: 2 ms (95.15%), Space: 45.6 MB (81.23%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,31 +1,22 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-
-        List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> current = new ArrayList<>();
-
-        backtrack(0, candidates, target, current, ans);
-
-        return ans;
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+         recur(candidates, 0, target, curr, res);
+        return res;
     }
-
-    public void backtrack(int i, int[] arr, int target, List<Integer> current, List<List<Integer>> ans) 
-                          {
- if (target == 0) 
- {
-            ans.add(new ArrayList<>(current));
-            return;
+        public void recur(int []candidates, int i,int target, List<Integer> curr, List<List<Integer>> res){
+            if(i==candidates.length){
+                if(target == 0){
+                res.add(new ArrayList<> (curr));
+                }
+                return;
+            }
+            if(candidates[i] <=target){
+                curr.add(candidates[i]);
+                recur(candidates, i, target-candidates[i], curr, res);
+                curr.remove(curr.size()-1);
+            }
+            recur(candidates, i+1, target, curr, res);
         }
-   if (i == arr.length || target < 0) 
-   {
-            return;
-        }
-
-          if (arr[i] <= target) {
-            current.add(arr[i]);
-            backtrack(i, arr, target - arr[i], current, ans);
-            current.remove(current.size() - 1);  
-        }
-     backtrack(i + 1, arr, target, current, ans);
     }
-}
