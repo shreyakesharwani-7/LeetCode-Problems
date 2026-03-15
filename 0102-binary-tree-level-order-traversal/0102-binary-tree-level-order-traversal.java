84709f9d29019daea9104ae1caba70113e72fe9c
Shreya Kesharwani <shreyakesharwani5524@gmail.com>
2026-03-15 14:42:29 +0530

    Time: 14 ms (2.25%), Space: 46.9 MB (36.38%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.java
@@ -14,29 +14,38 @@
  * }
  */
 class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ans = new ArrayList<>();    
-        if(root == null) 
-            return ans;
-        Queue<TreeNode> q = new LinkedList<>();
-        q.add(root);
 
-        while(!q.isEmpty()){
-            int size = q.size();
-            List<Integer> res = new ArrayList<>();
-            for(int i = 0; i < size; i++){
-                TreeNode curr = q.remove();
-                res.add(curr.val);
-                if(curr.left != null)
-                    q.add(curr.left);
-                
-                if(curr.right != null)
-                    q.add(curr.right);
-            }
+    public int height(TreeNode root) {
+        if(root==null){
+            return 0;
 
-            ans.add(res);
+        }
+        return 1 +Math.max(height(root.left),height(root.right));
         }
 
+    public void nthlevel(TreeNode root, int n, List<Integer> arr){
+        if(root == null)
+            return;
+        if(n==1){
+            arr.add(root.val);
+            return;
+        }
+        nthlevel(root.left, n-1, arr);
+        nthlevel(root.right, n -1, arr);
+    }
+
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        int level = height(root);
+        List<List<Integer>> ans = new ArrayList<>();
+        if(root == null){
+            return ans;
+        }
+        for(int i =1;i<=level;i++){
+            List<Integer> arr = new ArrayList<>();
+
+                nthlevel(root,i,arr);
+                ans.add(arr);
+        }
         return ans;
     }
 }
